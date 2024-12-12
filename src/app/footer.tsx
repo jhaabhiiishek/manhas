232db3951cd16fd0b0c77fe4eda6a712ff854bfc
Abhishek Jha <abhi10092003@gmail.com>
2024-12-12 09:33:14 +0530

    First version ready

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import twitter from "../../public/twitter.png"
 import facebook from "../../public/facebook.png"
 import instagram from "../../public/instagram.png"
+import Image from "next/image"
 
 export default function Footer() {
   return (
@@ -16,13 +17,13 @@ export default function Footer() {
       />
       <div className="mt-6" style={{ display: "flex", justifyContent: "center", gap: "15px", marginBottom: "10px" }}>
         <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-          <img src={twitter.src} alt="Twitter" style={{ width: "20px",filter:"invert(1)" }} />
+          <Image src={twitter.src} alt="Twitter" style={{ width: "20px",filter:"invert(1)" }}/>
         </a>
         <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-          <img src={facebook.src} alt="Facebook" style={{ width: "20px",filter:"invert(1)" }} />
+          <Image src={facebook.src} alt="Facebook" style={{ width: "20px",filter:"invert(1)" }} />
         </a>
         <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-          <img src={instagram.src} alt="Instagram" style={{ width: "20px",filter:"invert(1)" }} />
+          <Image src={instagram.src} alt="Instagram" style={{ width: "20px",filter:"invert(1)" }} />
         </a>
       </div>
       <div className="mt-6" style={{color:"white"}}>© Copyright {new Date().getFullYear()}, All Rights Reserved</div>
